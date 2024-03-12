38687c1b3e9cde890a26411563f39cca7ac7d448
UrPapuh <andrewgr073@gmail.com>
2024-03-12 20:11:57 +0000

    totally unsolved

--- src/views/LibraryView.vue
+++ src/views/LibraryView.vue
@@ -1,3 +1,36 @@
+<script setup>
+import { ref } from 'vue';
+
+const searchKey = ref('');
+</script>
+
 <template>
-  <h1>This is an library page</h1>
+  <div class="container">
+    <h2>Busqueda avanzada</h2>
+    <input
+      type="text"
+      placeholder="Search..."
+      class="form-control"
+      v-model="searchKey"
+    >
+    <br>
+    <table class="table table-bordered table-striped">
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Authors</th>
+          <th>Publisher</th>
+          <th>Subjects</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>title</td>
+          <td>authors</td>
+          <td>publisher</td>
+          <td>categories</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
