d67a2b806a03de476373cadd411bfd0b32672234
UrPapuh <andrewgr073@gmail.com>
2024-03-14 02:02:18 +0000

    author, publisher and category filter

--- src/views/LibraryView.vue
+++ src/views/LibraryView.vue
@@ -3,20 +3,31 @@
 
 import { watch , computed, ref } from 'vue';
 
-const searchKey = ref('');
+const keyFilter = ref('');
+const authorFilter = ref('');
+const publisherFilter = ref('');
+const categoryFilter = ref('');
+
 const searchResults = ref({});
-const filters = ref('');
 
 const searchFilters = computed (() => {
-  return searchKey.value + filters.value;
+  let filters = keyFilter.value;
+  if (authorFilter.value) filters += ` inauthor:${authorFilter.value}`;
+  if (publisherFilter.value) filters += ` inpublisher:${publisherFilter.value}`;
+  if (categoryFilter.value) filters += ` subject:${categoryFilter.value}`;
+// console.log(filters);
+  return filters;
 });
 
 watch(searchFilters, async (q) => {
   // FALTA METERLE LOS FILTROS
   if (q) {
-    q = q.replace(/ /g, '+');
+    q = q.trim().replace(/ /g, '+');
 
-    let response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${q}&fields=items(id,volumeInfo(title,authors,publisher,categories))&maxResults=10&printType=books`);
+    let url = `https://www.googleapis.com/books/v1/volumes?q=${q}&fields=items(id,volumeInfo(title,authors,publisher,categories))&maxResults=10&printType=books`;
+// console.log(url);
+
+    let response = await fetch(url);
     let json = await response.json();
     // console.log(response.status);
 
@@ -41,11 +52,66 @@ watch(searchFilters, async (q) => {
       type="text"
       placeholder="Search..."
       class="form-control mb-4"
-      v-model="searchKey"
+      v-model="keyFilter"
     >
-    <input type="text" v-model="filters" class="form-control" disabled>
-    <div class="table-responsive" v-if="searchKey && searchResults">
-      <table class="table table-bordered text-center">
+    <!-- <div id="accordion">
+      <div class="card">
+        <div class="card-header" id="heading1">
+          <h5 class="mb-0">
+            <button class="btn collapsed" data-toggle="collapse" data-target="#collapse1" aria-expanded="true" aria-controls="collapse1">Author</button>
+          </h5>
+        </div>
+        <div id="collapse1" class="collapse show" aria-labelledby="heading1" data-parent="#accordion">
+          <div class="card-body">
+            <input
+              type="text"
+              placeholder="Search..."
+              class="form-control"
+              v-model="authorFilter"
+            >
+          </div>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="card-header" id="heading2">
+          <h5 class="mb-0">
+            <button class="btn collapsed" data-toggle="collapse" data-target="#collapse2" aria-expanded="true" aria-controls="collapse2">Publisher</button>
+          </h5>
+        </div>
+        <div id="collapse2" class="collapse show" aria-labelledby="heading2" data-parent="#accordion">
+          <div class="card-body">
+            <input
+              type="text"
+              placeholder="Search..."
+              class="form-control"
+              v-model="publisherFilter"
+            >
+          </div>
+        </div>
+      </div>
+
+      <div class="card">
+        <div class="card-header" id="heading3">
+          <h5 class="mb-0">
+            <button class="btn collapsed" data-toggle="collapse" data-target="#collapse3" aria-expanded="true" aria-controls="collapse3">Category</button>
+          </h5>
+        </div>
+        <div id="collapse3" class="collapse show" aria-labelledby="heading3" data-parent="#accordion">
+          <div class="card-body">
+            <input
+              type="text"
+              placeholder="Search..."
+              class="form-control"
+              v-model="categoryFilter"
+            >
+          </div>
+        </div>
+      </div>
+    </div> -->
+    <!-- content -->
+    <div class="table-responsive" v-if="searchFilters && searchResults">
+      <table class="table table-bordered">
         <thead class="thead-dark">
           <tr>
             <th scope="col">Title</th>
@@ -81,7 +147,7 @@ watch(searchFilters, async (q) => {
         </tbody>
       </table>
     </div>
-    <div v-else class="container-fluid">
+    <div v-else class="container-fluid p-4">
       <span>No hay resultados</span>
     </div>
   </div>
