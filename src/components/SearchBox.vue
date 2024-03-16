20cfaca21ac2a83442dc416f9bef0f0493e7ecd8
UrPapuh <andrewgr073@gmail.com>
2024-03-16 18:49:23 +0000

    layouts updates and unsolved searchbox-modal

--- src/components/SearchBox.vue
+++ src/components/SearchBox.vue
@@ -1,7 +1,9 @@
-<script setup>
-
-</script>
-
 <template>
-  <h1>Searchbox</h1>
+  <!-- <div class="form-group has-search">
+    <span class="form-control-feedback">
+      <img src="@/assets/icons/search-icon.svg" alt="">
+    </span>
+    <input type="text" class="form-control" placeholder="Search">
+  </div> -->
 </template>
+
